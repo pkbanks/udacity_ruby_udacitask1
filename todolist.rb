616b243d1d02a1a94f948ab32c858aa09a924aed
PK Banks <pkbanks@gmail.com>
2016-04-10 13:24:15 -0400

    finished basic parameters of the problem set.  now moving on to supplemental features.

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,95 @@
 class TodoList
-    # methods and stuff go here
+	attr_reader :title, :items
+
+  # methods and stuff go here
+  def initialize(list_title)
+  	@title = list_title
+  	@items = Array.new
+  end
+
+  def title=(title)
+  	# update title
+  	@title = title
+  end
+
+  def add_item(new_item)
+  	# add an item
+  	item = Item.new(new_item)
+  	@items.push(item)
+  end
+
+  def remove_item(index)
+  	# removes item at index, returns the item
+  	return @items.delete_at(index)
+  end
+
+  def print(include_header=true)
+  	# returns a string that can be puts or exported to a file
+  	# optionally includes header
+  	result = ""
+
+  	result += get_header if include_header
+
+  	for item in @items do
+  		result += item.print
+  	end
+  	
+  	return result + "\n"
+  
+  end
+
+  def to_s
+  	# makes it easy to print the list, merely by 'puts list' command
+  	self.print
+  end
+
+  def get_header(char = "-", num = 4)
+  	# char is a string
+  	# num is an integer
+  	# char and num are character settings for the header that flank the title
+  	return line_break(char, num) + "  " + @title + "  " + line_break(char, num) + "\n"
+  end
+
 end
 
 class Item
+
+		attr_reader :description, :completed_status
+		
     # methods and stuff go here
+    def initialize(description)
+    	@description = description
+    	@completed_status = false
+    end
+
+    def update_status
+    	# toggles the status between true/false
+    	@completed_status = !@completed_status	
+    end
+
+    def to_s
+    	@description
+    end
+
+    def print
+    	# returns a string
+    	# designed for printing item to a list
+    	status = "ToDo"
+  		status = "Done" if @completed_status
+    	result = ''
+    	result += "#{status}\t#{@description}\n"
+    end
+
 end
+
+# ------------ helper methods ------------
+
+def line_break(char='*', num=15, carriage_return=false)
+	# returns a string of num chars
+	# designed to easily puts a line break
+	result = ''
+	num.times { result += char }
+	result += "\n" if carriage_return 
+	return result
+end
+
--- udacityask.rb
+++ udacityask.rb
@@ -1,23 +1,46 @@
 require_relative 'todolist.rb'
 
-# Creates a new todo list
 
+# Creates a new todo list
+list = TodoList.new('My To Do List')
+items = []
+item_descriptions = [
+	'feed the dog',
+	'pay bills',
+	'submit ruby project',
+	'make dinner plans'
+]
 # Add four new items
+for description in item_descriptions
+	list.add_item(Item.new(description))
+end
 
 # Print the list
+puts list
+
 
 # Delete the first item
+list.remove_item(0)
 
 # Print the list
+puts list
 
 # Delete the second item
+list.remove_item(1)
 
 # Print the list
+puts list
 
 # Update the completion status of the first item to complete
+list.items[0].update_status
 
 # Print the list
+puts list
 
 # Update the title of the list
+list.title = 'new title'
 
 # Print the list
+puts list
+
+
